eb54256548e3dc212d1e8083aa6d024bfe6b3cc7
pmir <przemekmirek@gmail.com>
2014-02-13 02:58:56 +0100

    ErrorDocument handling, minor syntax fixes

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<?php
+	$uri = $_SERVER['REQUEST_URI'];
+
+	if ($uri == "/index.php")
+	{
+		header("Location: /");
+		return;
+	}
+
+	if ($uri != "/")
+	{
+		header("Location: /error/404.html");
+		return;
+	}
+?>
+<!DOCTYPE html>
+<html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<title>GAKKO.EDU</title>
