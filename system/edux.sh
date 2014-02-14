fa1b04d985637f497796dff32c631603636cfb9d
JustArchi <JustArchi@JustArchi.net>
2014-02-14 01:54:37 +0100

    Edux Backend 1.2

--- system/edux.sh
+++ system/edux.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 # Edux backend
 
-#set -e
+set -e
 
 eduxoneshot() {
+	case "$1" in
+		"START") ;;
+		"STOP") ;;
+		*) return 1
+	esac
+	local MODE="$1"
 	local TASKS=""
 
 	TASKS+="eduxsftp "
-	SFTP="sftp.pjwstk.edu.pl"
 
-	echo "EDUXONESHOT: Calling tasks: $TASKS"
 	if [ $(echo "$TASKS" | wc -w) -gt 0 ]; then
 		for TASK in $TASKS; do
-			$TASK &
+			echo "EDUXONESHOT: Calling task: $TASK"
+			$TASK "$MODE"
 		done
 	fi
 	return 0
@@ -26,11 +31,11 @@ eduxcron() {
 	#ourBranch=$(git rev-parse --abbrev-ref HEAD)
 
 	#TASKS+="eduxsftp "
-	echo "EDUXCRON: Calling tasks: $TASKS"
 	if [ $(echo "$TASKS" | wc -w) -gt 0 ]; then
 		while :; do
 			for TASK in $TASKS; do
-				$TASK &
+				echo "EDUXCRON: Calling task: $TASK"
+				$TASK "START" &
 			done
 			sleep 60
 		done
@@ -42,31 +47,90 @@ eduxupdate() {
 	git pull $ourRepo $ourBranch
 }
 
-eduxsftp() {
-	# Check if our mirror is mounted already
-	if [ $(mount | grep $SFTP | wc -l) -eq 0 ]; then
-		local remoteMountPoint="public"
-		echo "EDUXSFTP: It looks like our SSHFS isn't available yet, mounting..."
-		echo "EDUXSFTP: User: $SFTPUSER"
-		if [ ! -z "$SFTPPASS" ]; then
-			echo "EDUXSFTP: Password is set!"
-		else
-			echo "EDUXSFTP: ERROR: Password is null!"
-			return 1
-		fi
-		echo "EDUXSFTP: Host: $SFTP"
-		echo "EDUXSFTP: Mount Point: $SCRIPTDIR/../sftp"
-		echo "EDUXSFTP: Remote Mount Point: $remoteMountPoint"
-		echo "EDUXSTFP: Mounting now!"
-		mkdir -p $SCRIPTDIR/../sftp
-		echo "$SFTPPASS" | sshfs -C -o ro -o reconnect -o auto_cache -o cache_timeout=86400 -o allow_other -o auto_unmount -o password_stdin -o ServerAliveInterval=60 -o ServerAliveCountMax=5 -o StrictHostKeyChecking=no $SFTPUSER@$SFTP:$remoteMountPoint $SCRIPTDIR/../sftp
-		echo "EDUXSFTP: Done!"
-		if [ $(mount | grep $SFTP | wc -l) -eq 0 ]; then
-			echo "EDUXSFTP: It looks like we failed, sad"
-		else
-			echo "EDUXSFTP: It looks like it works fine!"
-		fi
+eduxstart() {
+	echo "EDUX: Starting all services!"
+	eduxoneshot "START"
+}
+
+eduxstop() {
+	echo "EDUX: Stopping all services!"
+	eduxoneshot "STOP"
+}
+
+eduxexit() {
+	eduxstop
+	echo "EDUX: Exiting!"
+	echo $(date)
+	kill 0 # Bye
+	exit 0
+}
+
+eduxwatcher() {
+	local WATCHFOLDER="$SCRIPTDIR/../../private/backend"
+	if [ ! -d $WATCHFOLDER ]; then
+		echo "EDUXWATCHER: ERROR, NO $WATCHFOLDER"
+		return 1
 	fi
+	echo "EDUXWATCHER: Active!"
+	while :; do
+		inotifywait -qe create $WATCHFOLDER
+		for i in "RESTART" "STOP"; do
+			if [ -e $WATCHFOLDER/$i ]; then
+				rm -f $WATCHFOLDER/$i
+				case "$i" in
+					"RESTART") eduxstop && eduxstart ;;
+					"STOP") eduxexit ;;
+				esac
+				break # This will break for loop, not while
+			fi
+		done
+		sleep 1
+	done
+}
+
+eduxsftp() {
+	local SFTP="sftp.pjwstk.edu.pl"
+	case "$1" in
+		"START")
+			if [ $(mount | grep $SFTP | wc -l) -eq 0 ]; then
+				local remoteMountPoint="public"
+				echo "EDUXSFTP: It looks like our SSHFS isn't available yet, mounting..."
+				if [ ! -z "$SFTPPASS" ]; then
+					echo "EDUXSFTP: Password is set!"
+				else
+					echo "EDUXSFTP: ERROR: Password is null!"
+					return 1
+				fi
+				echo "EDUXSFTP: User: $SFTPUSER + Host: $SFTP + Mount Point: $SCRIPTDIR/../sftp + Remote Mount Point: $remoteMountPoint"
+				echo "EDUXSTFP: Mounting now!"
+				mkdir -p $SCRIPTDIR/../sftp
+				echo "$SFTPPASS" | sshfs -C -o ro -o reconnect -o auto_cache -o cache_timeout=86400 -o allow_other -o auto_unmount -o password_stdin -o ServerAliveInterval=60 -o ServerAliveCountMax=5 -o StrictHostKeyChecking=no $SFTPUSER@$SFTP:$remoteMountPoint $SCRIPTDIR/../sftp
+				echo "EDUXSFTP: Done!"
+				if [ $(mount | grep $SFTP | wc -l) -eq 0 ]; then
+					echo "EDUXSFTP: It looks like we failed mounting, sad"
+				else
+					echo "EDUXSFTP: It looks like it mounted fine!"
+				fi
+			else
+				echo "EDUXSFTP: It looks like we have mounted SFTP already!"
+			fi
+		;;
+		"STOP")
+			if [ $(mount | grep $SFTP | wc -l) -eq 0 ]; then
+				echo "EDUXSFTP: SFTP is unmounted already!"
+			else
+				echo "EDUXSTFP: Unmounting now!"
+				kill $(pidof sshfs)
+				echo "EDUXSFTP: Done!"
+				sleep 1
+				if [ $(mount | grep $SFTP | wc -l) -eq 0 ]; then
+					echo "EDUXSFTP: It looks like it unmounted fine!"
+				else
+					echo "EDUXSFTP: It looks like we failed unmounting!"
+				fi
+			fi
+		;;
+	esac
 }
 
 # Initial variables
@@ -85,11 +149,9 @@ USER=$(cat /etc/passwd | grep -i "$USER" | grep -i "/bin/bash" | head -n 1 | cut
 # Turn on logging
 echo -n "" > $SCRIPTDIR/../../private/edux.log
 chown $USER $SCRIPTDIR/../../private/edux.log
+
 exec 1>$SCRIPTDIR/../../private/edux.log
 exec 2>&1
-echo "EDUX: Welcome!"
-echo "EDUX: Detected user $USER"
-echo "EDUX: Detected folder $SCRIPTDIR"
 
 # Parse args
 for arg in $@; do
@@ -99,14 +161,12 @@ for arg in $@; do
 done
 
 # If we're called as root, fix it, drop privileges
-echo "EDUX: Called as $(whoami)"
 if [ $(whoami) != "$USER" ] && [ $(whoami) != "$ORIGUSER" ]; then
 	# Only root can read user and pass, so let's store it for future use
 	rm -f /tmp/edux
 	echo $(grep "user" $SCRIPTDIR/../../private/sftp.pass | cut -d'=' -f2) >> /tmp/edux
 	echo $(grep "pass" $SCRIPTDIR/../../private/sftp.pass | cut -d'=' -f2) >> /tmp/edux
 	chown $USER /tmp/edux
-	echo "su $USER -c "bash $0""
 	su $USER -c "bash $0 &"
 	exit 0
 fi
@@ -120,6 +180,11 @@ fi
 # Set traps to prevent leaving zombies behind
 trap "kill 0" SIGINT SIGTERM EXIT
 
+echo "EDUX: Welcome!"
+echo $(date)
+echo "EDUX: Detected user $USER"
+echo "EDUX: Detected folder $SCRIPTDIR"
+
 # Now when we're ready, we can make use of our user and pass
 SFTPUSER=$(sed -n 1p /tmp/edux)
 SFTPPASS=$(sed -n 2p /tmp/edux)
@@ -127,11 +192,14 @@ rm -f /tmp/edux
 
 # Call all services
 cd $SCRIPTDIR/..
-eduxoneshot
+eduxstart
 eduxcron &
+eduxwatcher &
 
-# Wait for cron to finish, which is unlikely to happen if at least one task is defined
+# Wait for all childs to finish, which is unlikely to happen if at least one task is defined
+echo "EDUX: Waiting for all childs to finish..."
 wait
 
 # Finish
+eduxexit
 exit 0
